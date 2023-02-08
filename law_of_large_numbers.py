49c4f4326394dc2e5de8ec75189377b60b83fb6e
Homer Liu <kingliu06@gmail.com>
2023-02-08 23:00:50 +0800

    add function and input

--- law_of_large_numbers.py
+++ law_of_large_numbers.py
@@ -3,19 +3,28 @@ from numpy.random import randn
 
 # 產生亂數
 def random_number(qq):
-	list = []
+	number = []
 	i= 0
 	while i < qq:
 		x = randn()
-		list.append(x)
+		number.append(x)
 		i += 1
+	return number
+
 # 計算機率
+def posibility(list_name):
 	in_stdev = 0 # 計算在一個標準差內的次數
-	for i in list:
+	for i in list_name: 
 		if i <=1 and i >= -1:
 			in_stdev += 1
-	result = (in_stdev / len (list))*100 # 在標準差內次數/總數
+	result = (in_stdev / len (list_name))*100 # 在標準差內次數/總數
 	print (result, '%')
 
-# main function
-random_number (10000000000)
+# 輸入與計算
+while True:
+	ask = input ('請輸入母體總數：')
+	if ask == 'q':
+		break
+	else:
+		s = random_number (int(ask)) # return存取的資料需要宣告變數
+		posibility (s)
